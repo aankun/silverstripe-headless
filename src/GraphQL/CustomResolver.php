4f0dda002c9e942391eabf26cfed898b9342d206
Andi Darmika <aankun.ganteng@gmail.com>
2023-07-09 10:56:25 +0800

    Update CustomResolver.php

--- src/GraphQL/CustomResolver.php
+++ src/GraphQL/CustomResolver.php
@@ -20,38 +20,7 @@ class CustomResolver
    * @return mixed|null
    * @see VersionedDataObject
    */
-
-  public static function resolveImageBySize(DataObject $obj, array $args, array $context, ResolveInfo $info)
-  {
-    $imageObj = Image::get()->byID($obj->ID);
-    $relativeUrl = '';
-    if ($imageObj) {
-      $processed = $imageObj;
-      $fieldName = $info->fieldName;
-      switch ($fieldName) {
-        case 'xlImage':
-          $processed = $imageObj->FitMax(1920, 1920);
-          break;
-        case 'lgImage':
-          $processed = $imageObj->FitMax(1080, 1080);
-          break;
-        case 'mdImage':
-          $processed = $imageObj->FitMax(800, 800);
-          break;
-        case 'smImage':
-          $processed = $imageObj->FitMax(640, 640);
-          break;
-        case 'xsImage':
-          $processed = $imageObj->FitMax(480, 480);
-          break;
-      }
-
-      $urls = explode('/assets/', $processed->AbsoluteURL);
-      $relativeUrl = '/assets/' . $urls[1];
-    }
-    return $relativeUrl;
-  }
-
+  
   public static function resolveBaseUrl(DataObject $obj, array $args, array $context, ResolveInfo $info)
   {
     $baseUrl = Environment::getEnv('NEXTJS_BASE_URL');
